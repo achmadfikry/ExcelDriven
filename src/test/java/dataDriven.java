cecb94c62a55faf63eafe26f4a20bde58aedaec7
telkomdev <telkomdev@telkomdevs-MacBook-Pro.local>
2025-08-07 20:48:22 +0700

    Read file to workbook, get sheet of workbook

---
+++ src/test/java/dataDriven.java
@@ -0,0 +1,23 @@
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class dataDriven {
+    public static void main(String[] args) throws IOException {
+
+        FileInputStream fis = new FileInputStream("/Users/telkomdev/Java/ExcelDriven/src/main/java/Framework/Main.java");
+        XSSFWorkbook workbook = new XSSFWorkbook(fis);
+
+        int sheets = workbook.getNumberOfSheets();
+        for (int i = 0; i < sheets; i++) {
+            if (workbook.getSheetName(i).equalsIgnoreCase("testdata")) {
+                XSSFSheet sheet = workbook.getSheetAt(i);
+            }
+
+        }
+
+    }
+}
