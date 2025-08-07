6f7f1bacd6cb24e76434e2014f2b3f6cd36928bd
telkomdev <telkomdev@telkomdevs-MacBook-Pro.local>
2025-08-07 21:23:05 +0700

    access first row & find desired column

--- src/test/java/dataDriven.java
+++ src/test/java/dataDriven.java
@@ -1,9 +1,12 @@
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Iterator;
 
 public class dataDriven {
     public static void main(String[] args) throws IOException {
@@ -15,6 +18,19 @@ public class dataDriven {
         for (int i = 0; i < sheets; i++) {
             if (workbook.getSheetName(i).equalsIgnoreCase("testdata")) {
                 XSSFSheet sheet = workbook.getSheetAt(i);
+
+                //Identify test case column by scanning the entire 1st row
+                Iterator<Row> row = sheet.iterator(); //this got an ability to move to each and every row. which sheet is collectio of rows
+                Row firstRow = row.next(); //move to / read the first row
+
+                Iterator<Cell> ce = firstRow.cellIterator(); //using Iterator cell. which row is collection of cells
+                while (ce.hasNext()) {
+                    Cell value = ce.next(); //move to / read the first cell
+                    if (value.getStringCellValue().equalsIgnoreCase("TestCases")){
+                        //desired column
+                    }
+                }
+
             }
 
         }
